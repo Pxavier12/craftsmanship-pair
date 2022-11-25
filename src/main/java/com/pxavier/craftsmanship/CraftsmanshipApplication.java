4a373c0fd5228fa910a4210ce35f6e17f2a2f82c
Pxavier12 <pxavier12@hotmail.fr>
2022-11-25 16:04:10 +0100

    first commit

---
+++ src/main/java/com/pxavier/craftsmanship/CraftsmanshipApplication.java
@@ -0,0 +1,13 @@
+package com.pxavier.craftsmanship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftsmanshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftsmanshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pxavier/craftsmanship/CraftsmanshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxavier.craftsmanship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftsmanshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
